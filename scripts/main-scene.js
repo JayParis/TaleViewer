1b55000c11603f9e1a99840757f6e5506af01401
JayParis <jay.paris32@gmail.com>
2023-07-03 13:45:16 +0100

    No WebGL

--- scripts/main-scene.js
+++ scripts/main-scene.js
@@ -1,93 +1,24 @@
-var MainScene = pc.createScript('Main-Scene-Script');
 
 let dlOffset = 1;
+var beganLoad = false;
 
-MainScene.attributes.add('speed', { 
-    type: 'number', default: 10 
-});
+var isMobile = navigator.maxTouchPoints > 0;
 
+if(isMobile){
+    document.addEventListener("touchstart", e => { inputDown(e); });
+    document.addEventListener("touchmove", e => { inputMove(e); });
+    document.addEventListener("touchend", e => { inputUp(e); });
+}else{
+    document.addEventListener("mousedown", e => { inputDown(e); });
+    document.addEventListener("mousemove", e => { inputMove(e); });
+    document.addEventListener("mouseup", e => { inputUp(e); });
+    console.log("assigned mouse events");
+}
+console.log(isMobile);
+
+
+/*
 MainScene.prototype.initialize = function() {
-    
-    plane.addComponent('render', {
-        type: 'plane'
-    });
-    app.root.addChild(plane);
-    plane.setEulerAngles(90,0,0);
-    
-    var testMat = new pc.BasicMaterial();
-    //testMat.color.set(0.76,1,0.915);
-    testMat.colorMap = assets.testTexture.resource;
-    plane.render.material = testMat;//model mat
-    
-    /*
-
-    var shaderDefinition = {
-        attributes: {
-            aPosition: pc.SEMANTIC_POSITION,
-            aUv0: pc.SEMANTIC_TEXCOORD0
-        },
-        vshader: assets.vs.resource,
-        fshader: assets.fs.resource
-    };
-    
-    var newShader = new pc.Shader(device, shaderDefinition);
-    //console.log(plane.render.meshInstances[0].material);
-    plane.render.meshInstances[0].material = new pc.Material();
-    plane.render.meshInstances[0].material.blendType = pc.BLEND_NORMAL;
-    plane.render.meshInstances[0].material.shader = newShader;
-    plane.render.meshInstances[0].material.setParameter('uDiffuseMap', assets.testTexture.resource);
-
-    */
-
-    topText.addComponent('element', {
-        pivot: new pc.Vec2(0.5, 0.5),
-        anchor: new pc.Vec4(0.5, 1, 0.5, 1),
-        type: pc.ELEMENTTYPE_TEXT,
-        font: assets.bpFont.resource,
-        fontSize: 22,
-        text: "Tale Viewer",
-        color: [0.01,0.01,0.01],
-        alignment: [0.5,0.5],
-    });
-    uiGroup.addChild(topText);
-    topText.setLocalPosition(0,-105,0);
-
-
-    loadButton.addComponent('element', {
-        type: pc.ELEMENTTYPE_IMAGE,
-        anchor: new pc.Vec4(1.0, 0.0, 1.0, 0.0),
-        width: 225,
-        height: 100,
-        //margin: new pc.Vec4(0.0, 0.0, 0.0, 0.0),
-        pivot: new pc.Vec2(0.5, 0.5), 
-        useInput: true,
-    });
-    uiGroup.addChild(loadButton);
-    loadButton.setLocalPosition(-145,145,0);
-    loadButton.addComponent('button', {
-        imageEntity: loadButton,
-    });
-    loadButton.button.on('click', function(evt){
-        console.log("Button pressed");
-        //loadRemoteImages();
-        loadImageURLs();
-        loadButton.button.active = false;
-    });
-
-
-
-    const light = new pc.Entity('light');
-    light.addComponent('light');
-    app.root.addChild(light);
-    light.setEulerAngles(45, 0, 0);
-    
-    //app.on('update', dt => plane.rotate(50 * dt, 60 * dt, 70 * dt));
-
-    //Device resize and orientation listeners
-    window.addEventListener('resize', () => this.resizeMobile());
-    window.addEventListener('orientationchange', () => this.resizeMobile());
-
-    this.resizeMobile();
 
     if(app.touch) {
         app.touch.on(pc.EVENT_TOUCHSTART, this.inputDown, this);
@@ -107,82 +38,40 @@ MainScene.prototype.initialize = function() {
         app.mouse.on(pc.EVENT_MOUSEMOVE, this.inputMove, this);
         app.mouse.on(pc.EVENT_MOUSEUP, this.inputUp, this);
     }
-
-    const onKeyDown = function (e) {
-        if (e.key === pc.KEY_F) {
-            console.log("Loading Remote Images");
-            //loadRemoteImages();
-            
-            //loadImageURLs();
-            
-            //loadedPage = false;
-        }
-        if (e.key === pc.KEY_T) {
-            allImagesReady();
-        }
-        if(e.key === pc.KEY_1){
-            plane.render.material.colorMap = app.assets.find("img_1","texture").resource;
-			plane.render.material.update();
-        }
-        if(e.key === pc.KEY_2){
-            plane.render.material.colorMap = app.assets.find("img_2","texture").resource;
-			plane.render.material.update();
-        }
-        if(e.key === pc.KEY_3){
-            plane.render.material.colorMap = app.assets.find("img_3","texture").resource;
-			plane.render.material.update();
-        }
-        if(e.key === pc.KEY_4){
-            plane.render.material.colorMap = app.assets.find("img_4","texture").resource;
-			plane.render.material.update();
-        }
-        if(e.key === pc.KEY_5){
-            plane.render.material.colorMap = app.assets.find("img_5","texture").resource;
-			plane.render.material.update();
-        }
-        if(e.key === pc.KEY_6){
-            plane.render.material.colorMap = app.assets.find("img_6","texture").resource;
-			plane.render.material.update();
-        }
-        e.event.preventDefault(); // Use original browser event to prevent browser action.
-    };
-    app.keyboard.on("keydown", onKeyDown, this);
-    
 };
+*/
 
-MainScene.prototype.inputDown = function(event) {
-    let screenX = app.touch ? event.touches[0].x : event.x;
-    let screenY = app.touch ? event.touches[0].y : event.y;
-
-    tapPosVal = camera.camera.screenToWorld(screenX,screenY,1);
-    tapPos.setPosition(tapPosVal);
-    holdPos.setPosition(tapPos);
-
+function inputDown(event) {
     inputting = true;
+
+    console.log(event);
+
+    tapPosVal = [event.x, event.y];
+
+    if(!beganLoad){
+        loadImageURLs();
+        beganLoad = true;
+    }
 }
 
-MainScene.prototype.inputMove = function(event) {
+function inputMove(event) {
     if(!inputting)
         return;
 
-    let screenX = app.touch ? event.touches[0].x : event.x;
-    let screenY = app.touch ? event.touches[0].y : event.y;
-
-    holdPosVal = camera.camera.screenToWorld(screenX,screenY,1);
-    holdPos.setPosition(holdPosVal);
 
     //topText.element.text = "tap: " + tapPosVal + "\nhold: " + holdPosVal;
 
     //currViewerID = Math.abs(Math.trunc((tapPosVal.x * vSens) - (holdPosVal.x * vSens)) % 15);
     //currViewerID = Math.abs((previousViewerID + Math.trunc((tapPosVal.x * vSens) - (holdPosVal.x * vSens))) % 15);
-    currViewerID = Math.abs(mod(previousViewerID + Math.trunc((tapPosVal.x * vSens) - (holdPosVal.x * vSens)), 160));
+    holdPosVal = [event.x, event.y];
+    
+    currViewerID = Math.abs(mod(previousViewerID + Math.trunc((tapPosVal[0] * vSens) - (holdPosVal[0] * vSens)), 160));
     
     if(loadedPage)
         viewer();
-
 }
 
-MainScene.prototype.inputUp = function(event) {
+function inputUp(event) {
     previousViewerID = currViewerID;
     inputting = false;
 
@@ -191,15 +80,8 @@ MainScene.prototype.inputUp = function(event) {
 }
 
 
-MainScene.prototype.update = function(dt) {
 
-};
-
-MainScene.prototype.swap = function(old) {
-
-};
-
- function viewer() {
+function viewer() {
     /*
     //topText.element.text = "ids: " + currViewerID;
 
@@ -232,16 +114,6 @@ MainScene.prototype.swap = function(old) {
     //ctx.drawImage(imageList[0],1,1);
     //console.log(imageList[0]);
 
-};
-
-MainScene.prototype.resizeMobile = function() {
-
-    let appHeight = document.getElementById('application').offsetHeight;
-    let appWidth = document.getElementById('application').offsetWidth;
-
-    let scale = camera.camera.orthoHeight * (appWidth / appHeight) * 2;
-
-    plane.setLocalScale(scale,1,scale * 1.25066);
 };
 
 function mod(n, m) {
@@ -294,7 +166,6 @@ function loadImageURLs(){
                 });
             })
     }
-    loadedPage = true;
     console.log("Finished Loading");
 }
 
@@ -318,6 +189,7 @@ function allImagesReady(){
     });
     
     console.log(imageList[0]);
+    loadedPage = true;
     
     //dlOffset++;
     //loadButton.button.active = true;
